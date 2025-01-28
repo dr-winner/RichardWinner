9a8829237a0416e3f211af12d1f92cdb8fa3f3c5
dr-winner <duvorrichardwinner@gmail.com>
2025-01-28 08:37:38 +0000

    Resolve sentry auth and shipped!

--- data/index.ts
+++ data/index.ts
@@ -90,7 +90,7 @@ export const projects = [
     id: 3,
     title: "HebronEdge - Corporate portfolio website",
     des: "Dedicated to empowering our clients' success by providing exceptional PR & media consultancy, event management, digital marketing, and IT services. We strive to be the catalyst that transforms our clients' aspirations into tangible, measurable results.",
-    img: "/p3.svg",
+    img: "/hebronedge.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
     link: "/https://hebronedgeinc.vercel.app/",
   },
@@ -173,14 +173,14 @@ export const testimonials = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Frontend Engineer",
+    desc: "Supervied the development of a web-based platform using React.js/Next.js enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
+    title: "Mobile App Dev - EduDesk Inc.",
     desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
@@ -194,8 +194,8 @@ export const workExperience = [
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Lead IT Specialist",
+    desc: "Oversaw and maintained IT systems and security operations using modern and up-to-date technologies.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
